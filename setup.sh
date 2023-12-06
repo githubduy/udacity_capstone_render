2cc5f3d016762a4a349ffbe72123ea0166763d21
Phan Van Duy <DuyPV3@fpt.com>
2023-12-07 00:07:07 +0700

    fix

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+pip install --upgrade pip
+apt install binutils gdal-bin libgdal-dev libproj-dev python3-dev python3-setuptools
 pip install setuptools
 pip install wheel
 pip install -r requirements.txt
