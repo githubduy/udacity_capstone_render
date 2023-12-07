4db136ed94f4db11db81bbba004102efcec1410d
Phan Van Duy <DuyPV3@fpt.com>
2023-12-07 22:18:22 +0700

    Remove some package pip

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@ pip install --upgrade pip
 sudo apt install -y binutils gdal-bin libgdal-dev libproj-dev python3-dev python3-setuptools
 #pip install setuptools
 sudo apt install -y build-essential
-pip install pyproject.toml
-pip install wheel
+pip install pyproject.toml tomlkit tomli
 pip install -r requirements.txt
