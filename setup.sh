9d7358e4c88a90bdc9477c5ed0ca96cce2f89570
Phan Van Duy <DuyPV3@fpt.com>
2023-12-07 08:43:24 +0700

    Add sudo to apt install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 pip install --upgrade pip
-apt install binutils gdal-bin libgdal-dev libproj-dev python3-dev python3-setuptools
+sudo apt install binutils gdal-bin libgdal-dev libproj-dev python3-dev python3-setuptools
 pip install setuptools
 pip install wheel
 pip install -r requirements.txt
